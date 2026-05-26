84083939acadca6e1b29498ec56ecef0894ac489
Shrushti Narnaware <shrushtinarnaware2@gmail.com>
2026-05-26 16:12:45 +0530

    Updated server port

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.post("/compile", async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
