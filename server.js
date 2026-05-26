05d94940c36432bd149b3299ae738669fc622dfb
Shrushti Narnaware <shrushtinarnaware2@gmail.com>
2026-05-26 16:31:11 +0530

    Fixed duplicate PORT

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("AI Compiler Backend Running");
